a6003f2163990a26458dff89b3d5546dc231c7fc
mrmcscreamypant <jrs.camper@gmail.com>
2024-07-27 18:52:39 -0400

    Working on building an executible

--- system/states/menu.py
+++ system/states/menu.py
@@ -1,8 +1,10 @@
+from pgzero.keyboard import keyboard
+
 from .state import State
-import CONSTS
 from system.screen import getscale
 
-instructions = []
+with open("./data/menu/instructions.txt") as file:
+    instructions = file.read().split("\n")
 
 class MenuState(State):
     frame = 0
@@ -10,15 +12,15 @@ class MenuState(State):
     def update(self):
         global instructions
 
-        if self.frame == 20:
-            instructions.append("MANSION")
+        if keyboard.x:
+            raise Exception("Hey, I havent gotten to that part yet!")
 
         self.frame += 1
 
     def draw_instructions(self):
         for y,text in enumerate(instructions):
             width,height = getscale()
-            self.screen.draw.text(text,center=(width/2,height/2+y*20),fontname="dosvga")
+            self.screen.draw.text(text,center=(width/2,height/2+y*20-len(instructions)*10),fontname="dosvga")
 
     def draw(self):
         super().draw()
